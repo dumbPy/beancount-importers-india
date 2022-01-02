fd6814bd59701a13401d8971d1f8c28d6a88be8e
Sufiyan Adhikari <sufiyan@streamingo.ai>
2022-01-02 22:12:29 +0530

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(os.path.join(current_directory, 'requirements.txt')) as f:
+    requirements = f.read()
+
+
 setup(
     name="beancount-importers-india",
     version="1.0.0",
@@ -13,5 +17,6 @@ setup(
     author="Sufiyan Adhikari",
     license='GPLv3',
     long_description=long_description,
-    python_requires='>3.8.0'
+    python_requires='>3.8.0',
+    install_requires=requirements
 )
