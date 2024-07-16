94569dfff898dcb9f6f30cec1bb9d156e1050823
Sufiyan Adhikari <dumbpyx@gmail.com>
2024-07-17 00:22:57 +0530

    update example config

--- example_config.py
+++ example_config.py
@@ -4,21 +4,19 @@ from typing import Tuple, Union, overload
 sys.path.insert(0, path.join(path.dirname(__file__)))
 
 from beancount_importers_india.SBI import Importer as SBIImporter
-from beancount_importers_india.HSBC import Importer as HSBCImporter
-from beancount_importers_india.PayTM import Importer as PayTMImporter
 from beancount_importers_india.BOI.BOI_enquiry_statement_docx import Importer as BOIImporter
-from beancount_importers_india.ICICI.csv_monthly import Importer as ICICICSVImporter
-from beancount_importers_india.ICICI.excel_yearly import Importer as ICICIFYImporter
+from beancount_importers_india.HSBC import Importer as HSBCCSVImporter
+from beancount_importers_india.PhonePe.transaction_email import PhonePeTransactionEmailImporter
+from beancount_importers_india.HDFC.creditcard import HDFCCreditCardEmailStatementImporter
 
 # from smart_importer import apply_hooks, PredictPayees, PredictPostings
 
 CONFIG = [
-        SBIImporter(30789863193, account="Assets:Saving:SBI"), # the account number is used to find the valid pdf
-        ICICICSVImporter(last4=6006, "Liability:CreditCard:ICICI"), # ICICI creditcard monthly csv importer
-        ICICIFYImporter(6006, "Liability:CreditCard:ICICI"), # ICICI creditcard yearly excel importer
-        HSBCImporter("Assets:Saving:HSBC"), # HSBC importer doesn't support account number based filtering, since account number is absent in csv
-        PayTMImporter(account_number=91123456780, "Assets:Saving:PayTM"), # Paytm bank importer
-        BOIImporter(91123456780, "Assets:Saving:BOI"), # Bank of India statement importer
+        SBIImporter(12345678901, account="Assets:Savings:SBI"), # the account number is used to find the valid pdf
+        HSBCCSVImporter("Assets:Savings:HSBC"), # HSBC importer doesn't support account number based filtering, since account number is absent in csv
+        BOIImporter(91123456780, "Assets:Savings:BOI"), # Bank of India statement importer
+        PhonePeTransactionEmailImporter(accounts_map_from_emails={'XXXX1234':'Assets:Savings:SBI', 'XXXX5678':'Assets:Savings:HSBC'}), # PhonePe transaction email notifications importer
+        HDFCCreditCardEmailStatementImporter(lines_to_grep=['Mr. JOHN DOE', 'XXXXXXXXXXXX1234'], password="password", account="Liabilities:CreditCard:HDFC") # HDFC Credit Card statement importer
 
         ]
 
